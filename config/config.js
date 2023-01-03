a2102ff6f196e843e832e69fc499690602788cf7
Larry Finol <larrysfinolb@gmail.com>
2023-01-02 22:08:54 -0400

    fix: rename enviroment

--- config/config.js
+++ config/config.js
@@ -4,12 +4,12 @@ const config = {
   env: process.env.NODE_ENV || 'development',
   isProd: process.env.NODE_ENV === 'production',
   port: process.env.PORT || '3000',
-  dbUser: process.env.DB_USER,
-  dbPassword: process.env.DB_PASSWORD,
-  dbHost: process.env.DB_HOST,
-  dbName: process.env.DB_NAME,
-  dbPort: process.env.DB_PORT,
-  dbUrl: process.env.DB_URL,
+  dbUser: process.env.PGUSER,
+  dbPassword: process.env.PGPASSWORD,
+  dbHost: process.env.PGHOST,
+  dbName: process.env.PGNAME,
+  dbPort: process.env.PGPORT,
+  dbUrl: process.env.DATABASE_URL,
 };
 
 module.exports = { config };
